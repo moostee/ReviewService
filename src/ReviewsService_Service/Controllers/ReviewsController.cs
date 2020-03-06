e0fc589fcae44dfe7725c187243fadaa8952b77b
Mustapha Omotunwase <mustapha.omotunwase@ubagroup.com>
2020-03-06 10:19:33 +0100

    edit review endpoint done

--- src/ReviewsService_Service/Controllers/ReviewsController.cs
+++ src/ReviewsService_Service/Controllers/ReviewsController.cs
@@ -49,5 +49,41 @@ namespace ReviewsService_Service.Controllers
             }
         }
 
+
+        /// <summary>
+        /// Update Review
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="form"></param>
+        /// <returns></returns>
+        [Route("Update")]
+        [HttpPost]
+        [Produces(typeof(ReviewModel))]
+        public IActionResult Update(Guid id, ReviewForm form)
+        {
+            var response = Utilities.InitializeResponse();
+            try
+            {
+                form.Id = id;
+                var model = Logic.ReviewLogic.Create(form);
+                if (id != model.Id)
+                    return BadRequest(Utilities.UnsuccessfulResponse(response, "Route Parameter does not match model ID"));
+                var found = Logic.ReviewLogic.Get(id);
+                if (found == null)
+                    return NotFound(Utilities.UnsuccessfulResponse(response, "Review not found"));
+                var check = Logic.ReviewLogic.UpdateExists(model);
+                if (check)
+                    return BadRequest(Utilities.UnsuccessfulResponse(response, "Review configuration already exists"));
+                response.Data = Logic.ReviewLogic.Update(found, model,
+                    "AppClientId,Comment,Rating,AppFeature,UserId,IsActive,ReviewTypeId,ParentId,RecordStatus");
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex);
+                return BadRequest(Utilities.CatchException(response, ex.Message));
+            }
+        }
+
     }
 }
